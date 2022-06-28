dfc94d0e8cde9f17bd2344c7e7c58eb2c010cb70
Myra Smith <myra.fowlkes.smith@gmail.com>
2022-06-28 18:16:25 -0400

    removes config data

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,8 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBk0WxCPGnDaDLgt30RvAuIboXAyOxLPb8",
-  authDomain: "clone-d2fac.firebaseapp.com",
-  projectId: "clone-d2fac",
-  storageBucket: "clone-d2fac.appspot.com",
-  messagingSenderId: "797474730271",
-  appId: "1:797474730271:web:bf58e314f0b36be7ab402a",
-  measurementId: "G-559PR9MQGV",
+  /* Replace with your own firebaseConfig object */
+  /* Find it on the project settings page for your firebase project */
 };
 
 // Initialize Firebase
