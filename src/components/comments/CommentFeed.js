1c7afd97b3163249abb68d95227c39edca6a7ad3
Myra Smith <myra.fowlkes.smith@gmail.com>
2022-06-28 19:32:41 -0400

    adds more comments boilerplate

--- src/components/comments/CommentFeed.js
+++ src/components/comments/CommentFeed.js
@@ -1,3 +1,29 @@
-export default function CommentFeed() {
-  return <h2>CommentFeed</h2>;
+import { useState } from "react";
+import CommentForm from "./CommentForm";
+import Comment from "./Comment";
+
+export default function CommentFeed({ videoId }) {
+  const [comments, setComments] = useState([]);
+  const [commentDraft, setCommentDraft] = useState({
+    author: "",
+    comment: "",
+  });
+
+  const handleSubmit = () => {};
+  const handleChange = () => {};
+
+  return (
+    <section>
+      <CommentForm
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        comment={commentDraft.comment}
+        author={commentDraft.author}
+      />
+
+      {comments.map((comment, idx) => (
+        <Comment key={idx} comment={comment} />
+      ))}
+    </section>
+  );
 }
--- src/components/comments/CommentForm.js
+++ src/components/comments/CommentForm.js
@@ -1,3 +1,28 @@
-export default function CommentForm() {
-  return <h2>CommentForm</h2>;
+export default function CommentForm({
+  handleChange,
+  handleSubmit,
+  comment,
+  author,
+}) {
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="author"
+        id="author"
+        placeholder="Author..."
+        value={author}
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="comment"
+        id="comment"
+        placeholder="Comment..."
+        value={comment}
+        onChange={handleChange}
+      />
+      <input type="submit" value="post comment" />
+    </form>
+  );
 }
